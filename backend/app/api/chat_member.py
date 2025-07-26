8c673eb14463926ee9d9c7a4439fac212146fdc0
NikolenkoRostislav <rostislavnikolenkowork@gmail.com>
2025-07-26 20:39:32 +0200

    refactor: cleaned up chat member service code

--- backend/app/api/chat_member.py
+++ backend/app/api/chat_member.py
@@ -5,22 +5,18 @@ from app.schemas import ChatMemberCreate, ChatMemberRead
 from app.services import ChatMemberService, ChatService, UserService
 from app.models import Chat, User, ChatMember
 from app.utils.auth import get_current_user
-from app.utils.exceptions import PermissionDeniedError, NotFoundError
+from app.utils.exceptions import PermissionDeniedError, NotFoundError, AlreadyExistsError
 
 router = APIRouter(prefix="/chat_member", tags=["chat_member"])
 
 @router.post("/join", response_model=ChatMemberRead)
 async def add_user_to_chat(user_id: int, chat_id: int, db: AsyncSession = Depends(get_db)):
-    chat = await ChatService.get_chat_by_id(db, chat_id)
-    user = await UserService.get_user_by_id(db, user_id)
-    if not chat:
-        raise HTTPException(status_code=404, detail="Chat not found")
-    if not user:
-        raise HTTPException(status_code=404, detail="User not found")
-    user_is_member = await ChatMemberService.get_chat_member_by_user_and_chat_id(user_id, chat_id, db)
-    if user_is_member:
-        raise HTTPException(status_code=400, detail="User already in chat")
-    return await ChatMemberService.add_user_to_chat(user_id, chat_id, db)
+    try:
+        return await ChatMemberService.add_user_to_chat(user_id, chat_id, db)
+    except NotFoundError as e:
+        raise HTTPException(status_code=404, detail=str(e))
+    except AlreadyExistsError as e:
+        raise HTTPException(status_code=400, detail=str(e))
 
 @router.get("/user-memberships/{user_id}", response_model=list[ChatMemberRead])
 async def get_chat_memberships(user_id: int, db: AsyncSession = Depends(get_db)):
@@ -35,7 +31,7 @@ async def get_chat_members(chat_id: int, db: AsyncSession = Depends(get_db)):
         return await ChatMemberService.get_chat_members_by_chat_id(chat_id, db)
     except NotFoundError as e:
         raise HTTPException(status_code=404, detail=str(e))
-        
+
 @router.delete("/remove-member")
 async def remove_member(user_id: int, chat_id: int, db: AsyncSession = Depends(get_db), current_user: User = Depends(get_current_user)):
     try: #I'll refactor the other routes to catch the exceptions from the services soon
--- backend/app/services/chat_member.py
+++ backend/app/services/chat_member.py
@@ -3,20 +3,9 @@ from sqlalchemy.future import select
 from app.models import Chat, User, ChatMember
 from app.schemas import ChatMemberCreate
 from app.services import ChatService, UserService
-from app.utils.exceptions import PermissionDeniedError, NotFoundError
+from app.utils.exceptions import PermissionDeniedError, NotFoundError, AlreadyExistsError
 
 class ChatMemberService:
-    @staticmethod
-    async def add_user_to_chat(user_id: int, chat_id: int, db: AsyncSession) -> ChatMember:
-        chat_member = ChatMember(
-            user_id=user_id,
-            chat_id=chat_id
-        )
-        db.add(chat_member)
-        await db.commit()
-        await db.refresh(chat_member)
-        return chat_member
-
     @staticmethod
     async def get_chat_member_by_id(chat_member_id: int, db: AsyncSession) -> ChatMember | None:
         result = await db.execute(select(ChatMember).where(ChatMember.id == chat_member_id))
@@ -42,6 +31,26 @@ class ChatMemberService:
     async def get_chat_members_by_user_id(user_id: int, db: AsyncSession) -> list[ChatMember]:#And replace this with a method in UserService
         result = await db.execute(select(ChatMember).where(ChatMember.user_id == user_id))
         return result.scalars().all()
+
+    @staticmethod
+    async def add_user_to_chat(user_id: int, chat_id: int, db: AsyncSession) -> ChatMember:
+        chat = await ChatService.get_chat_by_id(db, chat_id)
+        user = await UserService.get_user_by_id(db, user_id)
+        if not chat:
+            raise NotFoundError("Chat not found")
+        if not user:
+            raise NotFoundError("User not found")
+        user_is_member = await ChatMemberService.get_chat_member_by_user_and_chat_id(user_id, chat_id, db)
+        if user_is_member:
+            raise AlreadyExistsError("User already in chat")
+        chat_member = ChatMember(
+            user_id=user_id,
+            chat_id=chat_id
+        )
+        db.add(chat_member)
+        await db.commit()
+        await db.refresh(chat_member)
+        return chat_member
 
     @staticmethod
     async def remove_member(user_id: int, chat_id: int, db: AsyncSession, current_user: User):
