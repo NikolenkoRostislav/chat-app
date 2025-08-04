5b9f93467805afdb8ecc477564ba0b4207c941f7
NikolenkoRostislav <rostislavnikolenkowork@gmail.com>
2025-08-04 12:54:40 +0200

    refactor: improve type handling for chat button

--- frontend/src/components/ChatButton.tsx
+++ frontend/src/components/ChatButton.tsx
@@ -1,9 +1,9 @@
 import RouteButton from "./RouteButton";
 
 export type ChatButtonType = {
-    id: string;
-    name: string;
-    icon_url: string;
+    chat_id: string;
+    chat_name: string;
+    chat_icon_url: string;
     last_message?: string;
 };
 
@@ -13,10 +13,10 @@ type Props = {
 
 export default function ChatButton({ chat_button }: Props) {
     return (
-        <RouteButton route={`chat/${chat_button.id}`}>
+        <RouteButton route={`chat/${chat_button.chat_id}`}>
             <div className="chat-button">
-                <img src={chat_button.icon_url} alt={`${chat_button.name} chat icon`} />
-                <h2>{chat_button.name}</h2>
+                <img src={chat_button.chat_icon_url} alt={`${chat_button.chat_name} chat icon`} />
+                <h2>{chat_button.chat_name}</h2>
                 {chat_button.last_message && <p className="last-message">{chat_button.last_message}</p>}
             </div>
         </RouteButton>
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -6,20 +6,27 @@ import type { ChatButtonType } from '../components/ChatButton';
 export default function Home() {
     const { t } = useTranslation();
 
-    const { data: chats, loading, error } = useAuthFetch("/chat-member/chats/me");
+    const { data: chats_data, loading, error } = useAuthFetch("/chat-member/chats/me");
     if (loading) return <p>Loading...</p>;
     if (error) return <p>Error: {error}</p>;
+
+    const chat_buttons: ChatButtonType[] = (chats_data as any[]).map((c) => ({
+        chat_id: c.id,
+        chat_name: `${c.name}`,
+        chat_icon_url: `${c.icon_url}`,
+        last_message: `Last message in Chat ${c.id}`
+    }));
 
     return (
         <main>
             <h1>{t('greeting')}</h1>
             <p>home page placegolder text</p>
-            {chats.map((chat: ChatButtonType, index: number) => (          
+            {chat_buttons.map((chat: ChatButtonType, index: number) => (          
             <ChatButton key={index} chat_button={{
-                id: chat.id,
-                name: `${chat.name}`,
-                icon_url: `${chat.icon_url}`,
-                last_message: `Last message in Chat ${chat.id}`
+                chat_id: chat.chat_id,
+                chat_name: chat.chat_name,
+                chat_icon_url: chat.chat_icon_url,
+                last_message: chat.last_message
             }}/> ))}
         </main>
     );
