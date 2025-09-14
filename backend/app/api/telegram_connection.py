e22763184321a115a793fcb2b7666748e43273b0
NikolenkoRostislav <rostislavnikolenkowork@gmail.com>
2025-09-14 21:23:07 +0200

    fix: fixed temp code generation not requiring authentification

--- backend/app/api/telegram_connection.py
+++ backend/app/api/telegram_connection.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 from app.services import TelegramConnectionService
 from app.utils.exceptions import handle_exceptions
 from app.db import DatabaseDep
+from app.utils.auth import CurrentUserDep
 
 router = APIRouter(prefix="/tg-connection", tags=["tg_connection"])
 
@@ -12,5 +13,5 @@ async def connect_user_to_bot(temp_code: str, telegram_chat_id: str, db: Databas
 
 @router.post("/temp-code")
 @handle_exceptions
-async def create_temp_code(user_id: int, db: DatabaseDep):
-    return await TelegramConnectionService.create_temp_code(user_id, db)
+async def create_temp_code(current_user: CurrentUserDep, db: DatabaseDep):
+    return await TelegramConnectionService.create_temp_code(current_user.id, db)
